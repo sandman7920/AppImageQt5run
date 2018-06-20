15c7a5ebc7cb56c5e141fb49895be3345aca9aac
Nikolay Zlatev <nik@astrapaging.com>
2018-06-20 22:39:27 +0300

    New method to call CoreApplication::libraryPaths

--- main.cpp
+++ main.cpp
@@ -4,11 +4,12 @@
 #include <string>
 #include <dlfcn.h>
 #include <unistd.h>
+#include <locale>
+#include <codecvt>
 
 #define DBG(a) if (debug) std::cerr << a
 
 int main(int /*argc*/, char *argv[]) {
-    bool debug = getenv("QT5_DEBUG_RUN") != nullptr;
     char *real_path;
     char *appDir = getenv("APPDIR");
 
@@ -17,25 +18,6 @@ int main(int /*argc*/, char *argv[]) {
     } else {
         real_path = realpath(argv[0], nullptr);
     }
-
-    if (real_path == nullptr) return 1;
-
-    if (getenv("QT_EXISTS") == nullptr) {
-        DBG("First run load system libQt5Core.so.5: ");
-        void *handle = dlopen("libQt5Core.so.5", RTLD_LAZY);
-        if (handle != nullptr) {
-            dlclose(handle);
-            setenv("QT_EXISTS", "yes", 1);
-            setenv("LD_PRELOAD", "libQt5Core.so.5", 1);
-            DBG("loaded\nSet LD_PRELOAD=libQt5Core.so.5\n");
-        } else {
-            DBG("not loaded\n");
-            setenv("QT_EXISTS", "no", 1);
-        }
-        DBG("restart") << std::endl;
-        return execve(real_path, argv, environ);
-    }
-    unsetenv("LD_PRELOAD");
 
     const std::string self(real_path);
 
@@ -49,18 +31,24 @@ int main(int /*argc*/, char *argv[]) {
     const std::string appPath = (pos != std::string::npos) ? self.substr(0, pos) : "";
 
     std::string qt_plugins = std::string(appPath).append("/../plugins");
-    char *QT_EXISTS = getenv("QT_EXISTS");
-    if (QT_EXISTS && strncmp(QT_EXISTS, "yes", 3) == 0) {
-        for (auto const &qstring: QCoreApplication::libraryPaths()) {
+    void *handle = dlopen("libQt5Core.so.5", RTLD_LAZY);
+    if (handle != nullptr) {
+        QStringList (*libraryPaths)();
+        *(void **) (&libraryPaths) = dlsym(handle, "_ZN16QCoreApplication12libraryPathsEv");
+        std::wstring str;
+        for (auto &s: (*libraryPaths)().toStdList()) {
             qt_plugins.push_back(':');
-            qt_plugins.append(qstring.toStdString());
+            auto p = s.data_ptr();
+            str.clear();
+            str.append(p->begin(), p->end());
+            qt_plugins.append(std::wstring_convert<std::codecvt_utf8<wchar_t>>().to_bytes(str));
         }
+        dlclose(handle);
     }
-    unsetenv("QT_EXISTS");
 
     setenv("QT_PLUGIN_PATH", qt_plugins.c_str(), 1);
 
-    if (debug) {
+    if (getenv("QT5_DEBUG_RUN")) {
         std::cerr << "QT_PLUGIN_PATH: " << qt_plugins << '\n'
                   << "EXECUTABLE: " << argv[0] << std::endl;
     }
